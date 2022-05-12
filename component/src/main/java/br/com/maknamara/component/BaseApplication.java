7cf43f0dbf6fdc483a1b0288cced741ee9db49b6
Ekto Maknamara de OLiveira e Silva <maknamara@gmail.com>
2022-05-12 11:25:39 -0300

    Melhorias

---
+++ component/src/main/java/br/com/maknamara/component/BaseApplication.java
@@ -0,0 +1,6 @@
+package br.com.maknamara.component;
+
+import android.app.Application;
+
+public class BaseApplication extends Application {
+}
--- component/src/main/java/br/com/maknamara/component/CustomHandlerThread.java
+++ component/src/main/java/br/com/maknamara/component/CustomHandlerThread.java
@@ -2,12 +2,19 @@ package br.com.maknamara.component;
 
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.Process;
 
 public class CustomHandlerThread extends HandlerThread {
+    //TODO Ler: https://medium.com/@ali.muzaffar/handlerthreads-and-why-you-should-be-using-them-in-your-android-apps-dc8bf1540341
+    //https://www.youtube.com/watch?v=n0mkYSjldeA
     private Handler handler;
 
+    public CustomHandlerThread() {
+        this("cht_" + System.currentTimeMillis());
+    }
+
     public CustomHandlerThread(String name) {
-        super(name);
+        super(name, Process.THREAD_PRIORITY_BACKGROUND);
         start();
     }
 
