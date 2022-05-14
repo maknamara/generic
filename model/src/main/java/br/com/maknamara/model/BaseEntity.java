e75b4285dfac5ff44c3bf5a743055b6f1744b329
Ekto Maknamara de OLiveira e Silva <maknamara@gmail.com>
2022-05-14 18:32:54 -0300

    Melhorias

--- model/src/main/java/br/com/maknamara/model/BaseEntity.java
+++ model/src/main/java/br/com/maknamara/model/BaseEntity.java
@@ -5,22 +5,17 @@ import android.os.Build;
 import androidx.annotation.RequiresApi;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.j256.ormlite.field.DatabaseField;
 
 import java.util.Date;
-
-import br.com.maknamara.component.LocalDateTimeDeserializer;
-import br.com.maknamara.component.LocalDateTimeSerializer;
 
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class BaseEntity {
     @DatabaseField(generatedId = true)
     public Long id;
     @DatabaseField
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    //@JsonSerialize(using = LocalDateTimeSerializer.class)
+    //@JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private Date registrationDate;
 
     @JsonIgnore
