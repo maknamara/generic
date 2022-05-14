84117a1c887626b479792de440e63c140d4a11f7
Ekto Maknamara de OLiveira e Silva <maknamara@gmail.com>
2022-05-14 17:34:33 -0300

    Melhorias

---
+++ component/src/main/java/br/com/maknamara/component/LocalDateTimeDeserializer.java
@@ -0,0 +1,20 @@
+package br.com.maknamara.component;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@RequiresApi(api = Build.VERSION_CODES.O)
+public class LocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
+    @Override
+    public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+        return LocalDateTime.parse(jsonParser.getText());
+    }
+}
--- model/src/main/java/br/com/maknamara/model/BaseEntity.java
+++ model/src/main/java/br/com/maknamara/model/BaseEntity.java
@@ -1,13 +1,25 @@
 package br.com.maknamara.model;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.j256.ormlite.field.DatabaseField;
 
 import java.util.Date;
 
+import br.com.maknamara.component.LocalDateTimeDeserializer;
+import br.com.maknamara.component.LocalDateTimeSerializer;
+
+@RequiresApi(api = Build.VERSION_CODES.O)
 public class BaseEntity {
     @DatabaseField(generatedId = true)
     public Long id;
     @DatabaseField
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private Date registrationDate;
 
     public boolean isNew() {
