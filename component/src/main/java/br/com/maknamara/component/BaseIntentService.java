5b9d254818125711edf3e47c5d48cc6dcf8af246
Ekto Maknamara de OLiveira e Silva <maknamara@gmail.com>
2022-07-02 21:51:46 -0300

    Melhorias

--- component/src/main/java/br/com/maknamara/component/BaseIntentService.java
+++ component/src/main/java/br/com/maknamara/component/BaseIntentService.java
@@ -1,6 +1,9 @@
 package br.com.maknamara.component;
 
 import android.app.IntentService;
+import android.content.pm.PackageManager;
+
+import androidx.core.content.ContextCompat;
 
 import br.com.maknamara.di.DI;
 
@@ -9,5 +12,9 @@ public abstract class BaseIntentService extends IntentService {
     public BaseIntentService() {
         super("BIS-" + System.currentTimeMillis());
         DI.inject(this);
+    }
+
+    protected boolean isPermissionGranted(String permission) {
+        return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED;
     }
 }
